d79581e93acd59a25f2a10c66238e936b7c57b9b
hon99oo <kanghonggoo@naver.com>
2022-02-17 23:46:05 +0900

    Create Main.java

---
+++ programmers/level1/체육복/Main.java
@@ -0,0 +1,25 @@
+package programmers.level1.체육복;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        int n = 5;
+        int[] lost = {2,4};
+        int[] reserve = {1,3,5};
+
+        int answer = solution(n,lost,reserve);
+        System.out.println(answer);
+    }
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i]-1==reserve[j]){
+
+                }
+            }
+        }
+        return answer;
+    }
+}
